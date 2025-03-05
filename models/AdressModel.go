55e371131c9b738420386216b897341e0e4877a7
ykram051 <wikam5050@gmail.com>
2025-03-05 02:02:30 +0000

    Update models

--- models/AdressModel.go
+++ models/AdressModel.go
@@ -1,9 +1,9 @@
 package models
 
 type Address struct {
-	Street     string `bun:"street"`
-	City       string `bun:"city"`
-	State      string `bun:"state"`
-	PostalCode string `bun:"postal_code"`
-	Country    string `bun:"country"`
+	Street     string `json:"street" bun:"street"`
+	City       string `json:"city" bun:"city"`
+	State      string `json:"state" bun:"state"`
+	PostalCode string `json:"postal_code" bun:"postal_code"`
+	Country    string `json:"country" bun:"country"`
 }
--- models/OrderModel.go
+++ models/OrderModel.go
@@ -9,9 +9,9 @@ import (
 type Order struct {
 	bun.BaseModel `bun:"table:orders"`
 	ID            int         `bun:",pk,autoincrement"`
-	CustomerID    int         `bun:",notnull"` // Foreign key to Customer
-	Customer      *Customer   `bun:"rel:belongs-to,join:customer_id=id"`
-	Items         []OrderItem `bun:"rel:has-many,join:id=order_id"`
+	UserID        int         `bun:",notnull"` // Foreign key to User (replaces CustomerID)
+	User          *User       `bun:"rel:belongs-to,join:user_id=id"` // Relationship to User
+	Items         []OrderItem `bun:"rel:has-many,join:id=order_id"`  // Relationship to OrderItem
 	TotalPrice    float64     `bun:",notnull"`
 	CreatedAt     time.Time   `bun:",nullzero,notnull,default:current_timestamp"`
 	Status        string      `bun:",notnull"`
